a034d0ba3056a02e0e77d8e18a0f7fb93c815193
meojaswi <ojaswi3236@gmail.com>
2026-01-15 12:08:02 +0530

    Replace old POTD files with Day01.cpp

--- Leetcode POTD/Day01.cpp
+++ Leetcode POTD/Day01.cpp
@@ -1,3 +1,4 @@
+//15Jan26
 //2943. Maximize Area of Square Hole in Grid
 /*https://leetcode.com/problems/maximize-area-of-square-hole-in-grid/description/?envType=daily-question&envId=2026-01-15
 */
